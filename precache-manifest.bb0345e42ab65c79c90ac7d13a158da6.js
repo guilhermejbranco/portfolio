f90c7c6fc38247d391eaf1203bd2cb5260cbceff
Guilherme Branco <guilhermejbranco@Guilhermes-MBP.lan>
2020-01-24 03:22:57 +0000

    Updates

---
+++ precache-manifest.bb0345e42ab65c79c90ac7d13a158da6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0943f7bf9ae2723b0da69fe7e90e1f14",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3d3a2c49fc2e9620fd85",
+    "url": "/portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ac54d86e7a61aa20bfd3",
+    "url": "/portfolio/static/css/main.1de5fd72.chunk.css"
+  },
+  {
+    "revision": "3d3a2c49fc2e9620fd85",
+    "url": "/portfolio/static/js/2.b661d3b6.chunk.js"
+  },
+  {
+    "revision": "5d83b32028c822b364148904e89bb0e7",
+    "url": "/portfolio/static/js/2.b661d3b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac54d86e7a61aa20bfd3",
+    "url": "/portfolio/static/js/main.66703195.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "38649b96e3f82fd82bb6f610223e339f",
+    "url": "/portfolio/static/media/cj.38649b96.png"
+  },
+  {
+    "revision": "9dcf9a9da417a5068cf2f99ee69c9dd7",
+    "url": "/portfolio/static/media/fmh.9dcf9a9d.png"
+  },
+  {
+    "revision": "9ba8af6a13e79ad9236b8d83b319d3dd",
+    "url": "/portfolio/static/media/guibitmap.9ba8af6a.gif"
+  },
+  {
+    "revision": "61b713339bb0c6730776ada506522b30",
+    "url": "/portfolio/static/media/ml.61b71333.png"
+  },
+  {
+    "revision": "ea1d45a4683f023128a143da82dade94",
+    "url": "/portfolio/static/media/news.ea1d45a4.png"
+  },
+  {
+    "revision": "815a26384af784c897bbef1658509b4b",
+    "url": "/portfolio/static/media/weather.815a2638.png"
+  },
+  {
+    "revision": "34a551382871e50d9bcd183bb465f44b",
+    "url": "/portfolio/static/media/xpress.34a55138.png"
+  },
+  {
+    "revision": "0987a6c70ae6714ec3c1634399d81f9a",
+    "url": "/portfolio/static/media/young.0987a6c7.png"
+  }
+]);
